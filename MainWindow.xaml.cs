89c236932ebcd50e29a3c989cdc1528201cb8f77
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-10 18:55:35 -0400

    Oof

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,14 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VichoRISC.Components;
 
 namespace VichoRISC {
 	/// <summary>
 	/// Main window
 	/// </summary>
 	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
-		}
+		public MainWindow() => this.InitializeComponent();
 	}
 }
