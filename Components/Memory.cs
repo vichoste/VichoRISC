bbbdce9c7d3c6d9c17d4b0d432b7f3848b4fe8e8
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-10 23:52:55 -0400

    Stuff

---
+++ Components/Memory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VichoRISC.Components {
+	/// <summary>
+	/// Defines the memory
+	/// </summary>
+	public struct Memory {
+
+	}
+}
