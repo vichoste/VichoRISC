4b650f1e127e0ca9783d3dfc93925c7577dd67bc
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-10 23:01:12 -0400

    More docs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,28 +23,67 @@ namespace VichoRISC {
 		/// Creates the main window
 		/// </summary>
 		public MainWindow() => this.InitializeComponent();
-
+		/// <summary>
+		/// Executes the new command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void New(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the open command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void Open(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the save command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void Save(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the save as command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void SaveAs(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the exit command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void Exit(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the run command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void Run(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the step by step command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void Step(object sender, ExecutedRoutedEventArgs e) {
 
 		}
+		/// <summary>
+		/// Executes the see memory command
+		/// </summary>
+		/// <param name="sender">Who sends the event</param>
+		/// <param name="e">Event arguments</param>
 		private void SeeMemory(object sender, ExecutedRoutedEventArgs e) {
 
 		}
