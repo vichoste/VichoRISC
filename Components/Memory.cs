7a28fce793cbac0b9e2b6a0b9a7f9d887177cef0
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-11 00:24:50 -0400

    Some memory stuff

--- Components/Memory.cs
+++ Components/Memory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,38 @@ namespace VichoRISC.Components {
 	/// <summary>
 	/// Defines the memory
 	/// </summary>
-	public struct Memory {
-
+	public class Memory {
+		/// <summary>
+		/// Instead of a long array that overflows, I'm using a hashtable that emulates RAM
+		/// </summary>
+		private Hashtable _Ram;
+		/// <summary>
+		/// Creates a memory
+		/// </summary>
+		public Memory() => this._Ram = new Hashtable();
+		/// <summary>
+		/// Gets a memory value
+		/// </summary>
+		/// <param name="address">Desired address</param>
+		/// <returns>Value of the address</returns>
+		public int this[int address] {
+			get => (int)this._Ram[address];
+			set {
+				if (!this._Ram.ContainsKey(address)) {
+					this._Ram.Add(address, value);
+				} else {
+					this._Ram[address] = value;
+				}
+			}
+		}
+		/// <summary>
+		/// Clears an address content in the memory
+		/// </summary>
+		/// <param name="address">Desired address</param>
+		public void Clear(int address) {
+			if (this._Ram.ContainsKey(address)) {
+				this._Ram.Remove(address);
+			}
+		}
 	}
 }
