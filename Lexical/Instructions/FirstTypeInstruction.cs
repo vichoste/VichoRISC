5cd325e36197f5c8e1f8eb99cf8c79b7fb48e91b
Vicente <thunderwearsvicho37@protonmail.com>
2020-08-13 15:53:28 -0400

    Second type instruction

--- Lexical/Instructions/FirstTypeInstruction.cs
+++ Lexical/Instructions/FirstTypeInstruction.cs
@@ -5,16 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace VichoRISC.Lexical.Instructions {
+	/// <summary>
+	/// Defines the first type of instruction
+	/// and, sub, mul, div, mod, and, or, lsl, lsr, asr: (\w+ r[0-9]+, r[0-9]+, r[0-9]+)|(\w+ r[0-9]+, r[0-9]+, #[0-9]+)
+	/// </summary>
 	public class FirstTypeInstruction : Instruction {
+		/// <summary>
+		/// Regiser for storing the result
+		/// </summary>
 		public string DestinationRegister { get; private set; }
+		/// <summary>
+		/// First operand
+		/// </summary>
 		public string FirstRegister { get; private set; }
-		public string SecondRegisterOrImmediate { get; private set; }
-		public bool IsSecondRegisterImmediate { get; private set; }
-		public FirstTypeInstruction(int lineNumber, string keyword, string destinationRegister, string firstRegister, string secondRegisterPrefix, string secondRegisterOrImmediate) : base(lineNumber, keyword) {
+		/// <summary>
+		/// Second operand
+		/// </summary>
+		public string SecondRegister { get; private set; }
+		/// <summary>
+		/// Third operand
+		/// </summary>
+		public string ThirdRegisterOrImmediate { get; private set; }
+		/// <summary>
+		/// Determines if the third operand is an immediate value
+		/// </summary>
+		public bool IsThirdRegisterImmediate { get; private set; }
+		/// <summary>
+		/// Creates a first type instruction
+		/// </summary>
+		/// <param name="lineNumber">Line number</param>
+		/// <param name="keyword">Instruction keyword</param>
+		/// <param name="destinationRegister">Destination register</param>
+		/// <param name="firstRegister">First operand</param>
+		/// <param name="secondRegister">Second operand</param>
+		/// <param name="thirdRegisterPrefix">Third operand prefix to determine if it's an immediate value</param>
+		/// <param name="thirdRegisterOrImmediate">Third operand</param>
+		public FirstTypeInstruction(int lineNumber, string keyword, string destinationRegister, string firstRegister, string secondRegister, string thirdRegisterPrefix, string thirdRegisterOrImmediate) : base(lineNumber, keyword) {
 			this.FirstRegister = firstRegister;
 			this.DestinationRegister = destinationRegister;
-			this.SecondRegisterOrImmediate = secondRegisterOrImmediate;
-			this.IsSecondRegisterImmediate = string.Compare(secondRegisterPrefix, "r") != 0;
+			this.ThirdRegisterOrImmediate = thirdRegisterOrImmediate;
+			this.IsThirdRegisterImmediate = string.Compare(thirdRegisterPrefix, "r") != 0;
 		}
 	}
 }
---
+++ Lexical/Instructions/SecondTypeInstruction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VichoRISC.Lexical.Instructions {
+	/// <summary>
+	/// Defines the first type of instruction
+	/// mov, not: ((mov|not) r[0-9]+, r[0-9]+)|((mov|not) r[0-9]+, #[0-9]+)
+	/// </summary>
+	public class SecondTypeInstruction : Instruction {
+		/// <summary>
+		/// Regiser for storing the result
+		/// </summary>
+		public string DestinationRegister { get; private set; }
+		/// <summary>
+		/// First operand
+		/// </summary>
+		public string FirstRegister { get; private set; }
+		/// <summary>
+		/// Second operand
+		/// </summary>
+		public string SecondRegisterOrImmediate { get; private set; }
+		/// <summary>
+		/// Determines if the second operand is an immediate value
+		/// </summary>
+		public bool IsSecondRegisterImmediate { get; private set; }
+		/// <summary>
+		/// Creates a first type instruction
+		/// </summary>
+		/// <param name="lineNumber">Line number</param>
+		/// <param name="keyword">Instruction keyword</param>
+		/// <param name="destinationRegister">Destination register</param>
+		/// <param name="firstRegister">First operand</param>
+		/// <param name="secondRegisterPrefix">Second operand prefix to determine if it's an immediate value</param>
+		/// <param name="secondRegisterOrImmediate">Second operand</param>
+		public SecondTypeInstruction(int lineNumber, string keyword, string destinationRegister, string firstRegister, string secondRegisterPrefix, string secondRegisterOrImmediate) : base(lineNumber, keyword) {
+			this.FirstRegister = firstRegister;
+			this.DestinationRegister = destinationRegister;
+			this.SecondRegisterOrImmediate = secondRegisterOrImmediate;
+			this.IsSecondRegisterImmediate = string.Compare(secondRegisterPrefix, "r") != 0;
+		}
+	}
+}
